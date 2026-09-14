03cc8c7a7833487d6b59d04a808680f50c8f7e56
Epic <tractionbruh@gmail.com>
2026-09-14 21:31:38 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,9 +1,9 @@
 const mineflayer = require('mineflayer')
 
 // ==== CONFIG (fill these in or use env vars) ====
-const HOST = process.env.MC_HOST || 'yourserver.aternos.me'
+const HOST = process.env.MC_HOST || 'veryevilserver.aternos.me'
 const PORT = parseInt(process.env.MC_PORT || '25565')
-const USERNAME = process.env.MC_USERNAME || '24/7bot'
+const USERNAME = process.env.MC_USERNAME || 'TheEpicGamer901'
 const VERSION = process.env.MC_VERSION || '1.21.11'
 // =================================================
 
